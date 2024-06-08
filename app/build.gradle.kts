a964a5b8311327548023d1f3c525efb18c9dedcc
nmthanh31 <nmthanh3107.03@gmail.com>
2024-06-09 03:07:21 +0700

    bottom navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -32,7 +33,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -40,4 +40,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-storage")
+    implementation("com.google.android.material:material:1.2.0")
 }
---
+++ app/src/main/java/com/example/myemployeedirectorymanagement/activity/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.myemployeedirectorymanagement.activity;
+
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.myemployeedirectorymanagement.R;
+import com.example.myemployeedirectorymanagement.fragment.DepartmentFragment;
+import com.example.myemployeedirectorymanagement.fragment.EmployeeFragment;
+import com.example.myemployeedirectorymanagement.fragment.SettingFragment;
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FrameLayout flFragment;
+
+    private BottomNavigationView bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
+            return insets;
+        });
+
+        flFragment = findViewById(R.id.flFragment);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        replaceFragment(new DepartmentFragment());
+
+
+        bottomNavigationView.setOnItemSelectedListener(item ->{
+            if (item.getItemId() == R.id.menuDepartment){
+                replaceFragment(new DepartmentFragment());
+                return true;
+            }
+            if (item.getItemId() == R.id.menuEmployee){
+                replaceFragment(new EmployeeFragment());
+                return true;
+            }
+            if (item.getItemId() == R.id.menuSetting){
+                replaceFragment(new SettingFragment());
+                return true;
+            }
+            return false;
+        });
+
+    }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.flFragment, fragment);
+        fragmentTransaction.commit();
+    }
+}
